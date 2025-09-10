aee0cd7435f8530a5e96de76a268d74caf2ccb1a
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-09-10 11:31:57 -0400

    cv iframe google doc

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -1,21 +1,19 @@
 // get tag from url, get post details
-import { defineQuery } from "next-sanity";
-import { PortableText } from "next-sanity";
-import { sanityFetch } from "@/sanity/live";
 import styles from "@/app/ui/page.module.css"
 
-const CV_QUERY = defineQuery(`
-  *[_type == "cv"][0]
-`);
+
 
 export default async function Cv() {
-  // fetch a rich text cv field from sanity
-  const { data: cvData } = await sanityFetch({ query: CV_QUERY });
+   
   return (
     <div className={styles.main}>
       <h1 className={styles.title}>Chloe Engel CV</h1>
       <div className={styles.cvBodyWrapper}>
-        <PortableText value={cvData.body}/> 
+        <iframe
+          src="https://docs.google.com/document/d/your-doc-id/pub?embedded=true"
+          style={{ width: "100%", height: "100%", border: "none" }}
+          title="Chloe Engel CV"
+        />
       </div>
     </div>
   );
